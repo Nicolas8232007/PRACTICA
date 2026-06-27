88e9dc80b6ae885197f209b2b7a0dfb6b629d126
Nicolas <nicomartineza23@gmail.com>
2026-06-27 02:02:52 -0500

    feat: create spring boot backend

---
+++ backend/src/test/java/com/nicolas/taskmanager/TaskManagerApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.nicolas.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
